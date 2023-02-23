be8bf1422d334e027542c170d29d91a51d8df46c
통합기술연구팀-신상호 <shshin@ex-em.com>
2023-02-23 17:17:06 +0900

    [sql] ae_db_info 조회 컬럼 추가

--- sql/common_sql.py
+++ sql/common_sql.py
@@ -4,7 +4,7 @@ class CommonSql:
 
     SELECT_AE_DB_INFO = (
         """
-        SELECT db_id FROM ae_db_info
+        SELECT db_id, instance_name FROM ae_db_info
         ORDER BY db_id asc
         """
     )

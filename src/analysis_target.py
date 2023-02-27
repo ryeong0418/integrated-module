0191e0af649a83bcf5112ae9b94a3549ff6f0ed6
ryeong0418 <ryeong2105@gmail.com>
2023-02-27 17:08:51 +0900

    delete duplicate date

--- src/analysis_target.py
+++ src/analysis_target.py
@@ -6,7 +6,7 @@ import psycopg2.extras
 from sqlalchemy import create_engine, text
 
 from src.common.utils import TargetUtils, SystemUtils
-from src.common.constants import TableConstants,SystemConstants
+from src.common.constants import TableConstants
 from sql.initialize_sql import InterMaxInitializeQuery, MaxGaugeInitializeQuery, SaInitializeQuery
 from sql.sql_text_merge_sql import InterMaxSqlTextMergeQuery, SaSqlTextMergeQuery
 from sql.extract_sql import InterMaxExtractQuery, MaxGaugeExtractorQuery
@@ -101,50 +101,60 @@ class InterMaxTarget(CommonTarget):
         table_name = TableConstants.AE_SQL_TEXT
         TargetUtils.default_insert_data(self.logger, self.analysis_engine, table_name, filtered_df)
 
+
     def insert_intermax_detail_data(self):
         self.sa_conn = db.connect(self.analysis_conn_str)
-        
-        self._set_insert_xapm_txn_detail()
-        
-        self._set_insert_xapm_txn_sql_detail()
-        
-        self._set_insert_xapm_txn_sql_fetch()
 
-    def _set_insert_xapm_txn_detail(self):
         date_conditions = TargetUtils.set_intermax_date(self.config['args']['s_date'], self.config['args']['interval'])
         for date in date_conditions:
             table_suffix_dict = {'table_suffix': date}
-            query = InterMaxExtractQuery.SELECT_XAPM_TXN_DETAIL
-            detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-            table_name = TableConstants.AE_TXN_DETAIL
-            try:
-                self._excute_insert_intermax_detail_data(detail_query, table_name)
-            except Exception as e:
-                self.logger.exception(e)
+            delete_query = InterMaxExtractQuery.DELETE_INTERMAX_QUERY
 
-    def _set_insert_xapm_txn_sql_detail(self):
-        date_conditions = TargetUtils.set_intermax_date(self.config['args']['s_date'], self.config['args']['interval'])
-        for date in date_conditions:
-            table_suffix_dict = {'table_suffix': date}
-            query = InterMaxExtractQuery.SELECT_XAPM_TXN_SQL_DETAIL
-            detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-            table_name = TableConstants.AE_TXN_SQL_DETAIL
-            try:
-                self._excute_insert_intermax_detail_data(detail_query, table_name)
-            except Exception as e:
-                self.logger.exception(e)
+            self._set_insert_xapm_txn_detail(table_suffix_dict,delete_query,date)
 
-    def _set_insert_xapm_txn_sql_fetch(self):
-        date_conditions = TargetUtils.set_intermax_date(self.config['args']['s_date'], self.config['args']['interval'])
-        for date in date_conditions:
-            table_suffix_dict = {'table_suffix': date}
-            query = InterMaxExtractQuery.SELECT_XAPM_TXN_SQL_FETCH
-            detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-            table_name = TableConstants.AE_TXN_SQL_FETCH
-            try:
-                self._excute_insert_intermax_detail_data(detail_query, table_name)
-            except Exception as e:
-                self.logger.exception(e)
+            self._set_insert_xapm_txn_sql_detail(table_suffix_dict,delete_query,date)
+
+            self._set_insert_xapm_txn_sql_fetch(table_suffix_dict,delete_query,date)
+
+    def _set_insert_xapm_txn_detail(self, table_suffix_dict, delete_query, date, table_name=TableConstants.AE_TXN_DETAIL):
+        query = InterMaxExtractQuery.SELECT_XAPM_TXN_DETAIL
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_dict = {'table_name': table_name, 'date': date}
+        im_delete_query = SystemUtils.sql_replace_to_dict(delete_query,delete_dict)
+        TargetUtils.default_sa_execute_query(self.logger,self.sa_conn,im_delete_query)
+
+        try:
+            self._excute_insert_intermax_detail_data(detail_query, table_name)
+
+        except Exception as e:
+            self.logger.exception(e)
+
+    def _set_insert_xapm_txn_sql_detail(self, table_suffix_dict, delete_query, date, table_name=TableConstants.AE_TXN_SQL_DETAIL):
+        query = InterMaxExtractQuery.SELECT_XAPM_TXN_SQL_DETAIL
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_dict = {'table_name': table_name, 'date': date}
+        im_delete_query = SystemUtils.sql_replace_to_dict(delete_query,delete_dict)
+        TargetUtils.default_sa_execute_query(self.logger,self.sa_conn,im_delete_query)
+
+        try:
+            self._excute_insert_intermax_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def _set_insert_xapm_txn_sql_fetch(self,table_suffix_dict,delete_query,date,table_name=TableConstants.AE_TXN_SQL_FETCH):
+        query = InterMaxExtractQuery.SELECT_XAPM_TXN_SQL_FETCH
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_dict = {'table_name': table_name, 'date': date}
+        im_delete_query = SystemUtils.sql_replace_to_dict(delete_query, delete_dict)
+        TargetUtils.default_sa_execute_query(self.logger,self.sa_conn, im_delete_query)
+
+        try:
+            self._excute_insert_intermax_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
 
     def _excute_insert_intermax_detail_data(self, query, table_name):
         df = TargetUtils.get_target_data_by_query(self.logger, self.im_conn, query, table_name)
@@ -191,100 +201,102 @@ class MaxGaugeTarget(CommonTarget):
     def insert_maxgauge_detail_data(self):
         self.sa_conn = db.connect(self.analysis_conn_str)
 
-        self._set_insert_ora_session_info()
-
-        self._set_insert_ora_session_stat()
-
-        self._set_insert_apm_sql_list()
-
-        self._set_insert_ora_sql_stat_10()
-
-        self._set_insert_ora_sql_wait_10()
-
-    def _set_insert_ora_session_info(self):
         date_conditions = TargetUtils.set_maxgauge_date(self.config['args']['s_date'], self.config['args']['interval'])
         ae_db_info_query = CommonSql.SELECT_AE_DB_INFO
         ae_db_info_name = TableConstants.AE_DB_INFO
+        delete_query = MaxGaugeExtractorQuery.DELETE_MAXGAUGE_QUERY
         df = TargetUtils.get_target_data_by_query(self.logger, self.sa_conn, ae_db_info_query, ae_db_info_name)
-        for i, name in df.values:
-            db_id = str(i).zfill(3)
-            for date in date_conditions:
-                table_suffix_dict = {'instance_name': name, 'partition_key':date+db_id}
-                query = MaxGaugeExtractorQuery.SELECT_ORA_SESSION_INFO
-                detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-                table_name = TableConstants.AE_SESSION_INFO
-                try:
-                    self._excute_insert_maxgauge_detail_data(detail_query, table_name)
-                except Exception as e:
-                    self.logger.exception(e)
 
-    def _set_insert_ora_session_stat(self):
-        date_conditions = TargetUtils.set_maxgauge_date(self.config['args']['s_date'], self.config['args']['interval'])
-        ae_db_info_query = CommonSql.SELECT_AE_DB_INFO
-        ae_db_info_name = TableConstants.AE_DB_INFO
-        df = TargetUtils.get_target_data_by_query(self.logger, self.sa_conn, ae_db_info_query, ae_db_info_name)
         for i, name in df.values:
             db_id = str(i).zfill(3)
             for date in date_conditions:
-                table_suffix_dict = {'instance_name': name, 'partition_key':date+db_id}
-                query = MaxGaugeExtractorQuery.SELECT_ORA_SESSION_STAT
-                detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-                table_name = TableConstants.AE_SESSION_STAT
-                try:
-                    self._excute_insert_maxgauge_detail_data(detail_query, table_name)
-                except Exception as e:
-                    self.logger.exception(e)
+                table_suffix_dict = {'instance_name': name, 'partition_key': date + db_id}
+                delete_suffix_dict = {'table_name': 'known', 'partition_key': date + db_id}
 
-    def _set_insert_apm_sql_list(self):
-        date_conditions = TargetUtils.set_maxgauge_date(self.config['args']['s_date'], self.config['args']['interval'])
-        ae_db_info_query = CommonSql.SELECT_AE_DB_INFO
-        ae_db_info_name = TableConstants.AE_DB_INFO
-        df = TargetUtils.get_target_data_by_query(self.logger, self.sa_conn, ae_db_info_query, ae_db_info_name)
-        for i, name in df.values:
-            db_id = str(i).zfill(3)
-            for date in date_conditions:
-                table_suffix_dict = {'instance_name': name, 'partition_key':date+db_id}
-                query = MaxGaugeExtractorQuery.SELECT_APM_SQL_LIST
-                detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-                table_name = TableConstants.AE_DB_SQL_TEXT
-                try:
-                    self._excute_insert_maxgauge_detail_data(detail_query, table_name)
-                except Exception as e:
-                    self.logger.exception(e)
+                self._set_insert_ora_session_info(table_suffix_dict,delete_query,delete_suffix_dict)
 
-    def _set_insert_ora_sql_stat_10(self):
-        date_conditions = TargetUtils.set_maxgauge_date(self.config['args']['s_date'], self.config['args']['interval'])
-        ae_db_info_query = CommonSql.SELECT_AE_DB_INFO
-        ae_db_info_name = TableConstants.AE_DB_INFO
-        df = TargetUtils.get_target_data_by_query(self.logger, self.sa_conn, ae_db_info_query, ae_db_info_name)
-        for i, name in df.values:
-            db_id = str(i).zfill(3)
-            for date in date_conditions:
-                table_suffix_dict = {'instance_name': name, 'partition_key':date+db_id}
-                query = MaxGaugeExtractorQuery.SELECT_ORA_SQL_STAT_10
-                detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-                table_name = TableConstants.AE_SQL_STAT_10MIN
-                try:
-                    self._excute_insert_maxgauge_detail_data(detail_query, table_name)
-                except Exception as e:
-                    self.logger.exception(e)
+                self._set_insert_ora_session_stat(table_suffix_dict,delete_query,delete_suffix_dict)
 
-    def _set_insert_ora_sql_wait_10(self):
-        date_conditions = TargetUtils.set_maxgauge_date(self.config['args']['s_date'], self.config['args']['interval'])
-        ae_db_info_query = CommonSql.SELECT_AE_DB_INFO
-        ae_db_info_name = TableConstants.AE_DB_INFO
-        df = TargetUtils.get_target_data_by_query(self.logger, self.sa_conn, ae_db_info_query, ae_db_info_name)
-        for i, name in df.values:
-            db_id = str(i).zfill(3)
-            for date in date_conditions:
-                table_suffix_dict = {'instance_name': name, 'partition_key':date+db_id}
-                query = MaxGaugeExtractorQuery.SELECT_ORA_SQL_WAIT_10
-                detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
-                table_name = TableConstants.AE_SQL_WAIT_10MIN
-                try:
-                    self._excute_insert_maxgauge_detail_data(detail_query, table_name)
-                except Exception as e:
-                    self.logger.exception(e)
+                self._set_insert_apm_sql_list(table_suffix_dict,delete_query,delete_suffix_dict)
+
+                self._set_insert_ora_sql_stat_10(table_suffix_dict,delete_query,delete_suffix_dict)
+
+                self._set_insert_ora_sql_wait_10(table_suffix_dict,delete_query,delete_suffix_dict)
+
+    def _set_insert_ora_session_info(self,table_suffix_dict,delete_query,delete_suffix_dict,table_name=TableConstants.AE_SESSION_INFO):
+
+        query = MaxGaugeExtractorQuery.SELECT_ORA_SESSION_INFO
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_suffix_dict['table_name'] = table_name
+
+        mg_delete_query = SystemUtils.sql_replace_to_dict(delete_query, delete_suffix_dict)
+        TargetUtils.default_sa_execute_query(self.logger, self.sa_conn, mg_delete_query)
+
+        try:
+            self._excute_insert_maxgauge_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def _set_insert_ora_session_stat(self,table_suffix_dict,delete_query,delete_suffix_dict,table_name=TableConstants.AE_SESSION_STAT):
+
+        query = MaxGaugeExtractorQuery.SELECT_ORA_SESSION_STAT
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_suffix_dict['table_name'] = table_name
+
+        mg_delete_query = SystemUtils.sql_replace_to_dict(delete_query, delete_suffix_dict)
+        TargetUtils.default_sa_execute_query(self.logger, self.sa_conn, mg_delete_query)
+
+        try:
+            self._excute_insert_maxgauge_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def _set_insert_apm_sql_list(self,table_suffix_dict,delete_query,delete_suffix_dict,table_name=TableConstants.AE_DB_SQL_TEXT):
+
+        query = MaxGaugeExtractorQuery.SELECT_APM_SQL_LIST
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_suffix_dict['table_name'] = table_name
+
+        mg_delete_query = SystemUtils.sql_replace_to_dict(delete_query, delete_suffix_dict)
+        TargetUtils.default_sa_execute_query(self.logger, self.sa_conn, mg_delete_query)
+
+        try:
+            self._excute_insert_maxgauge_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def _set_insert_ora_sql_stat_10(self,table_suffix_dict,delete_query,delete_suffix_dict,table_name=TableConstants.AE_SQL_STAT_10MIN):
+
+        query = MaxGaugeExtractorQuery.SELECT_ORA_SQL_STAT_10
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_suffix_dict['table_name'] = table_name
+
+        mg_delete_query = SystemUtils.sql_replace_to_dict(delete_query, delete_suffix_dict)
+        TargetUtils.default_sa_execute_query(self.logger, self.sa_conn, mg_delete_query)
+
+        try:
+            self._excute_insert_maxgauge_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def _set_insert_ora_sql_wait_10(self,table_suffix_dict,delete_query,delete_suffix_dict,table_name=TableConstants.AE_SQL_WAIT_10MIN):
+
+        query = MaxGaugeExtractorQuery.SELECT_ORA_SQL_WAIT_10
+        detail_query = SystemUtils.sql_replace_to_dict(query, table_suffix_dict)
+
+        delete_suffix_dict['table_name'] = table_name
+
+        mg_delete_query = SystemUtils.sql_replace_to_dict(delete_query, delete_suffix_dict)
+        TargetUtils.default_sa_execute_query(self.logger, self.sa_conn, mg_delete_query)
+
+        try:
+            self._excute_insert_maxgauge_detail_data(detail_query, table_name)
+        except Exception as e:
+            self.logger.exception(e)
 
     def _excute_insert_maxgauge_detail_data(self, query, table_name):
         df = TargetUtils.get_target_data_by_query(self.logger, self.mg_conn, query, table_name,)

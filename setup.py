eccaafd83955d1dccd34415ec84bf505f72e5edb
통합기술연구팀-신상호 <shshin@ex-em.com>
2023-03-13 17:06:47 +0900

    [공통] setup.py 추가

---
+++ setup.py
@@ -0,0 +1,43 @@
+import glob
+import os
+import time
+
+from setuptools import setup, find_packages
+from src.pypackage_meta import __title__, __version__
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('package/requirements.txt', 'r', encoding='utf-8') as f:
+    requires = f.read().splitlines()
+
+if not os.path.isdir("package/setup"):
+    os.mkdir("package/setup")
+
+[os.remove(f) for f in glob.glob('package/setup/*.*')]
+
+os.system("cd package&&pip download -r requirements.txt -d ./setup")
+
+time.sleep(1)
+
+setup(
+    name=__title__,
+    version=__version__,
+    author='exem',
+    author_email='exem@ex-em.com',
+    description="Exam Data Analyzer",
+    long_description=readme,
+    packages=find_packages(exclude=['tests']),
+    install_requires=requires,
+    python_requires=">=3.8",
+    package_dir={
+        "resources": "resources"
+    },
+    package_data={
+        "resources": ['*/*-prod.json'],
+        "": ['../*.py',
+             '../export/sql_csv/sql/*.txt',
+             '../package/requirements.txt',
+             '../package/setup/*']
+    }
+)

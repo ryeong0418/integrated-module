17707cd0beee5c4c51441e43b755082a722fc956
통합기술연구팀-신상호 <shshin@ex-em.com>
2023-03-15 13:27:17 +0900

    [공통] meta 디렉토리 변경

--- src/meta/pypackage_meta.py
+++ src/meta/pypackage_meta.py
@@ -1,5 +1,5 @@
 """Define meta information about module package."""
 
-__title__ = 'Smart_Analyzer'
+__title__ = 'Smart_analyzer'
 __version__ = '1.0.0'
 __copyright__ = 'Copyright 2023 Exem Corp. All rights reserved.'

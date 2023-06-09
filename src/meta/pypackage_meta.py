4512d32fa56f5ddc3f624edaf98dc28646612e67
통합기술연구팀-신상호 <shshin@ex-em.com>
2023-06-09 11:12:02 +0900

    [meta] version

--- src/meta/pypackage_meta.py
+++ src/meta/pypackage_meta.py
@@ -1,5 +1,5 @@
 """Define meta information about module package."""
 
 __title__ = 'Smart_analyzer'
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __copyright__ = 'Copyright 2023 Exem Corp. All rights reserved.'

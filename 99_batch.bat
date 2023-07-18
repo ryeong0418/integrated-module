0601d879e065346b176853a553a30bcb1661c215
통합기술연구팀-신상호 <shshin@ex-em.com>
2023-07-18 17:04:49 +0900

    [script] batch 스크립트 수정

--- 99_batch.bat
+++ 99_batch.bat
@@ -1,23 +1,4 @@
 @echo off
 
-PUSHD %~DP0
-
-for %%i in (%~dp0..) do set ParentPath=%%~fi
-
-type "%ParentPath%\exem_analysis_module.txt"
-
-SET result="unknown error"
-
 call .venv\Scripts\activate
-python smart_analyzer.py --proc b > Output
-
-SET /p result=<Output
-
-echo.
-echo %result%
-echo.
-
-DEL Output
-
-pause
-
+python smart_analyzer.py --proc b
